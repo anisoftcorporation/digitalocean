3fd03632251e43e7376a0f931de2787ce8447639
abhattacharya <abhattacharya@paypal.com>
2015-10-22 14:58:35 -0700

    Added log

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-	 console.log
+	 console.log("New Conn");
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('More Interesting Stuffs to Appear');
 }).listen(9090,'0.0.0.0');
-console.log('Server running at http://104.236.240.241:9090/');
+console.log('Server running at http://104.236.240.241:9090/'); 
