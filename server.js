c8d28d07cb1c45817d3d95f5fc4b45b158f6587c
abhattacharya <abhattacharya@paypal.com>
2015-10-22 14:21:06 -0700

    again added 0000

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ http.createServer(function (req, res) {
 	 console.log
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('More Interesting Stuffs to Appear');
-}).listen(9999);
-console.log('Server running at http://104.236.240.241:8080/');
+}).listen(9090,'0.0.0.0');
+console.log('Server running at http://104.236.240.241:9090/');
