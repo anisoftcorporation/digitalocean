b89aa30d6fb9e851f2c83ab5f62fa073b8f23be0
abhattacharya <abhattacharya@paypal.com>
2015-10-22 15:02:03 -0700

    Changing content type

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
 	 console.log("New Conn");
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('More Interesting Stuffs to Appear');
 }).listen(9090,'0.0.0.0');
 console.log('Server running at http://104.236.240.241:9090/'); 
