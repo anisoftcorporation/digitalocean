80de36e46683a23816e4d4a0799185edaf663125
abhattacharya <abhattacharya@paypal.com>
2015-10-22 15:16:22 -0700

    res.write

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
 	 console.log("New Conn");
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('More Interesting Stuffs to Appear');
+	res.write('More Interesting Stuffs to Appear');
+    res.end();
 }).listen(8080,'0.0.0.0');
-console.log('Server running at http://104.236.240.241:9090/'); 
+console.log('Server running at http://104.236.240.241:8080/'); 
