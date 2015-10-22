b73fc804ad5bf12b864cec81cae3748266dc6569
abhattacharya <abhattacharya@paypal.com>
2015-10-22 13:07:52 -0700

    updated code

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-//Lets require/import the HTTP module
 var http = require('http');
 
-//Lets define a port we want to listen to
-const PORT=8080; 
-
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('More Interesting Things to Come Here');
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT,'0.0.0.0', function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
-});
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('More Interesting Stuffs to Appear');
+}).listen(8080, "0.0.0.0");
+console.log('Server running at http://0.0.0.0:8080/');
