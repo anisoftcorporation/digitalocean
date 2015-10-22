cd32b9c5a52a8d36a56ae7920fec30865fbab012
abhattacharya <abhattacharya@paypal.com>
2015-10-22 13:14:00 -0700

    Putting static Ip

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('More Interesting Stuffs to Appear');
-}).listen(8080, "0.0.0.0");
-console.log('Server running at http://0.0.0.0:8080/');
+}).listen(8080, "104.236.240.241");
+console.log('Server running at http://104.236.240.241:8080/');
