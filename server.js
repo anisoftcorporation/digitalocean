b1ad10f672a04356f7a762e68b632f7e88d89d2e
abhattacharya <abhattacharya@paypal.com>
2015-10-22 14:09:47 -0700

    Removing host

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ http.createServer(function (req, res) {
 	 console.log
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('More Interesting Stuffs to Appear');
-}).listen(9999, "0.0.0.0");
+}).listen(9999);
 console.log('Server running at http://104.236.240.241:8080/');
