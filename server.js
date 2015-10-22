5274200a8e526b5ed5c65ed1de7c3b15ba2a8e39
abhattacharya <abhattacharya@paypal.com>
2015-10-22 13:04:20 -0700

    Update IP

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ function handleRequest(request, response){
 var server = http.createServer(handleRequest);
 
 //Lets start our server
-server.listen(PORT, function(){
+server.listen(PORT,'0.0.0.0', function(){
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });
