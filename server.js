6bf15517e8b1ed78ef5c10f43314441f72fcc5be
abhattacharya <abhattacharya@paypal.com>
2015-10-22 13:20:14 -0700

    changed again

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+	 console.log
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('More Interesting Stuffs to Appear');
-}).listen(8080, "104.236.240.241");
+}).listen(8080, "0.0.0.0");
 console.log('Server running at http://104.236.240.241:8080/');
