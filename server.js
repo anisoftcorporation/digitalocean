5c968df892578247a044ecdefddd5efc8ba1ec5a
abhattacharya <abhattacharya@paypal.com>
2015-10-26 14:13:05 -0700

    package.json

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
+app.get('/chat', function (req, res) {
+  res.send('Chat!');
+});
+app.get('/login', function (req, res) {
+  res.send('Login!');
+});
 var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
